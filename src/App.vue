634294928dee803bb336faf828e9a40857af2793
Caldissa <kayla@prarieskies.com>
2024-04-16 15:44:11 -0500

    removed small joke

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,6 @@
     <div
         class="container mx-auto max-w-prose p-2 min-h-screen flex items-center justify-center"
     >
-        <h1 class="text-5xl tracking-widest">HI MOM</h1>
+        <h1 class="text-5xl tracking-widest">INVENTORY</h1>
     </div>
 </template>
