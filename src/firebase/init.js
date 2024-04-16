2407dcdf89933e76c3cac0e2cd86ae93c5c4a47d
Caldissa <kayla@prarieskies.com>
2024-04-16 03:06:59 -0500

    added firebase

---
+++ src/firebase/init.js
@@ -0,0 +1,20 @@
+import { initializeApp } from 'firebase/app'
+import { getAnalytics } from 'firebase/analytics'
+import { getFirestore } from 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: 'AIzaSyB5ts9fRzt9afJzTYLo66xrpHI6Y7i5yoI',
+    authDomain: 'pathfinder-inventory.firebaseapp.com',
+    projectId: 'pathfinder-inventory',
+    storageBucket: 'pathfinder-inventory.appspot.com',
+    messagingSenderId: '962715164223',
+    appId: '1:962715164223:web:628bf7e3ed685bb77c228a',
+    measurementId: 'G-R9VEXK6S8Y'
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+const analytics = getAnalytics(app)
+const db = getFirestore(app)
+
+export { db }
