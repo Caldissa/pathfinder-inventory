cca1b1181bb111984526855fa6838953bd7b75c4
Caldissa <kayla@prarieskies.com>
2024-04-23 15:15:34 -0500

    Updated header to use router

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,10 +1,12 @@
 <template>
     <div class="p-4 shadow-lg bg-blue-600 text-white flex justify-between">
         <h1 class="text-5xl tracking-widest">PRM</h1>
-        <div></div>
+        <div v-if="route.path != '/login'">Menu</div>
     </div>
 </template>
 
 <script setup lang="ts">
-import router from '../router'
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
 </script>
