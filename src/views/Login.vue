872dcba963de9bb47bf5b6236c25bbbfcb2f9688
Caldissa <kayla@prarieskies.com>
2024-04-23 16:18:38 -0500

    Began login page

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,3 +1,12 @@
 <template>
-    <h2>Login</h2>
+    <div class="h-full w-full flex justify-center items-center">
+        <div
+            class="mb-30 p-6 shadow-lg border-[1px] rounded-lg flex justify-center flex-col gap-4"
+        >
+            <h2 class="text-4xl tracking-wider font-semibold">Login</h2>
+            <button class="bg-blue-600 text-white rounded-md">
+                <i class="i-mdi:arrow-right w-12 h-12"></i>
+            </button>
+        </div>
+    </div>
 </template>
