2b88a4c6093f3b98c55c6a15618db5a23ecb95ae
Caldissa <kayla@prarieskies.com>
2024-04-29 22:18:25 -0500

    modified home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,10 +12,13 @@
             <div
                 class="bg-white text-3xl font-semibold p-6 shadow-lg border-[1px] rounded-lg flex text-center justify-center flex-col gap-4"
             >
-                <h3>INVENTORY</h3>
-                <RouterLink to="/attendance" class="button secondary p-2">
+                <h3 class="opacity-50">INVENTORY</h3>
+                <div class="button disabled p-2">
                     <i class="i-mdi:arrow-right w-8 h-8"></i>
-                </RouterLink>
+                </div>
+                <!-- <RouterLink to="/inventory" class="button secondary p-2">
+                    <i class="i-mdi:arrow-right w-8 h-8"></i>
+                </RouterLink> -->
             </div>
         </div>
     </div>
